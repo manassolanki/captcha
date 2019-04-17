fdc85fb28f997d6894f908c707798495ec7f1c9b
Manas Solanki <manassolanki@gmail.com>
2019-04-18 00:10:56 +0530

    [WIP] Added the index in the mongodb for email

--- captcha/database.py
+++ captcha/database.py
@@ -1,4 +1,5 @@
 import click
+
 from flask import current_app, g
 from flask.cli import with_appcontext
 
@@ -27,8 +28,22 @@ def close_db(e=None):
         db.close()
 
 
+def add_index():
+    # add the mongodb index on email id
+    get_db().users.createIndex({"email": 1}, {"unique": True})
+
+
+@click.command('add-db-index')
+@with_appcontext
+def add_db_index():
+    """Clear existing data and create new tables."""
+    add_index()
+    click.echo('Added the database index on email.')
+
+
 def init_app(app):
     """Register database functions with the Flask app. This is called by
     the application factory.
     """
-    app.teardown_appcontext(close_db)
+    # app.teardown_appcontext(close_db)
+    app.cli.add_command(add_db_index)
